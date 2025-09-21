5562b9e71740af80fb49b250270d553b1214fcfb
chetry01 <chetryanmun01@gmail.com>
2025-09-21 21:09:09 +0530

    changed print statement

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,2 +1,2 @@
 public class HelloWorld 
-{ public static void main(String[] args) { System.out.println("Hello, World!"); } }
+{ public static void main(String[] args) { System.out.println("My name is Aisha"); } }
